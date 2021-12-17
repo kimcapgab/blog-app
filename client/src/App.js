475b34639771973cd72b73e91a190e2bbf00013f
Rgarf <nothanks@gmail.com>
2021-12-17 15:58:35 -0500

    Layout component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-
+import {Routes, Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<h1>Home</h1>} />
+        <Route path='/posts' element={<h1>all posts</h1>} />
+        <Route path='/detail/:id' element={<h1>details</h1>} />
+        <Route path='/create' element={<h1>create</h1>} />
+        <Route path='/edit/:id' element={<h1>edit</h1>}/>
+      </Routes>
     </div>
   );
 }
