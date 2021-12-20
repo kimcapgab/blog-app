759a84c342e970cda4cb2e0a263d871e66eb0c12
Mike2217 <msoles2217@gmail.com>
2021-12-20 15:45:17 -0500

    Navbar and Christmas

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
 
       <Routes>
         <Route path='/' element={<h1>Home</h1>} />
-        <Route path='/posts' element={<h1>all posts</h1>} />
+        <Route path='/posts' element={<h1>All Posts</h1>} />
         <Route path='/detail/:id' element={<Details />} />
         <Route path='/create' element={<Create />} />
         <Route path='/edit/:id' element={<h1>edit</h1>}/>
