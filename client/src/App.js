b73280b2af1d3dbf7dd2c59a17a67ac9a6a7c874
Rgarf <nothanks@gmail.com>
2021-12-20 11:51:30 -0500

    fixed posts.js and added details component

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom'
 import './App.css';
 import Create from './screens/Create/Create';
+import Details from './screens/details/Details';
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
       <Routes>
         <Route path='/' element={<h1>Home</h1>} />
         <Route path='/posts' element={<h1>all posts</h1>} />
-        <Route path='/detail/:id' element={<h1>details</h1>} />
-        <Route path='/create' element={<Create/>} />
+        <Route path='/detail/:id' element={<Details />} />
+        <Route path='/create' element={<Create />} />
         <Route path='/edit/:id' element={<h1>edit</h1>}/>
       </Routes>
     </div>
