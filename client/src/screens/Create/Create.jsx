1925c419a24b6022824f9209d60732168af6b00d
Mike2217 <msoles2217@gmail.com>
2021-12-20 16:41:15 -0500

    Create CSS

--- client/src/screens/Create/Create.jsx
+++ client/src/screens/Create/Create.jsx
@@ -1,6 +1,7 @@
 import Layout from "../../components/Layout/Layout"
 import { useState } from 'react'
 import { postNewPosts } from "../../posts"
+import './Create.css'
 
 
 export default function Create() {
@@ -23,14 +24,20 @@ export default function Create() {
 
   return (
     <Layout>
-      <h1>Create Your Post</h1>
+      <div id='background'>
+      <div id='CreateForm'>Create Your Post</div>
+      <div id='form-container'>
+      <div id='form'>
       <form onSubmit={handleSubmit}>
-        <input type='text' name='name' value={name} onChange={(e)=>setName(e.target.value)}/>
-        <input type='text' name='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
-        <input type='textarea' name='post' value={post} onChange={(e)=>setPost(e.target.value)}/>
-        <input type='text' name='img' value={img} onChange={(e) => setImg(e.target.value)} />
+        <input className='form-field' placeholder='Name' type='text' name='name' value={name} onChange={(e)=>setName(e.target.value)}/>
+        <input className='form-field' placeholder='Title' type='text' name='title' value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <input className='form-field' placeholder='Post' type='textarea' name='post' value={post} onChange={(e)=>setPost(e.target.value)}/>
+        <input className='form-field' placeholder='ImageURL' type='text' name='img' value={img} onChange={(e) => setImg(e.target.value)} />
         <button>Submit This Post</button>
-      </form>
+        </form>
+        </div>
+        </div>
+        </div>
     </Layout>
   )
 }
